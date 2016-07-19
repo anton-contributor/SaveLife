249c2ebe49436bce00ac23604c7e6a0c95465cec
Anton <fishing94@mail.ru>
2016-07-19 16:36:08 +0300

    Version 1.0

---
+++ spring-mvc/src/main/java/mvc/controller/MainController.java
@@ -0,0 +1,18 @@
+package mvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by anton on 19.07.16.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/"})
+    public String print(Model model){
+        return "index";
+    }
+}
